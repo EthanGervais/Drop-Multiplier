2bd7a59c251b9d48c9f0d508349523fc6afd0af3
EthanGervais <EthanGervais@users.noreply.github.com>
2020-11-16 19:50:49 -0500

    Base Plugin Setup
    The plugin files are setup and ready to be coded.

---
+++ MultiplyDrops/src/com/ethangervais/plugin/MultiplyDrops.java
@@ -0,0 +1,16 @@
+package com.ethangervais.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MultiplyDrops extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
