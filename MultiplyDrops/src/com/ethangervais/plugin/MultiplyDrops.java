fd6edafe3702d0cf9ffbf3b9390b28c13e1b0f7a
EthanGervais <EthanGervais@users.noreply.github.com>
2020-11-16 20:59:51 -0500

    Incremental Drops
    The item drops now increment by 1 for each block that is broken.

--- MultiplyDrops/src/com/ethangervais/plugin/MultiplyDrops.java
+++ MultiplyDrops/src/com/ethangervais/plugin/MultiplyDrops.java
@@ -1,16 +1,45 @@
 package com.ethangervais.plugin;
 
+import java.util.Collection;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MultiplyDrops extends JavaPlugin {
-	
+public class MultiplyDrops extends JavaPlugin implements Listener {
+	private int dropAmount = 1;
+
 	@Override
 	public void onEnable() {
-		
+		getServer().getPluginManager().registerEvents(this, this);
 	}
-	
+
 	@Override
 	public void onDisable() {
+
+	}
+
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event) {
+		Block block = event.getBlock();
+		Location loc = block.getLocation();
+
+		Collection<ItemStack> drops = block.getDrops();
 		
+		loc.getBlock().setType(Material.AIR);
+
+		for (ItemStack drop : drops) {
+			for (int i = 0; i < dropAmount; i++) {
+				loc.getWorld().dropItemNaturally(loc, new ItemStack(drop));
+			}
+		}
+
+		event.setCancelled(true);
+		dropAmount++;
 	}
 }
